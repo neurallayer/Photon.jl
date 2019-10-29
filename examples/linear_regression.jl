0045de559080404a7e5a09140a90f9fb57633d9b
neurallayer <info@jbaron.com>
2019-10-29 14:51:25 +0100

    improved losses

---
+++ examples/linear_regression.jl
@@ -0,0 +1,29 @@
+using Photon, Printf
+
+
+"""
+The most simple model possible, just a single layer with 1 output
+"""
+model = Dense(1)
+
+
+"""
+This is the function we want to approximate
+"""
+fn(x,y,z) = 2x - 3y + 4z .- 5
+
+
+"""
+Now we create the training data
+"""
+X = [randn(3,8) for _ in 1:50]
+Y = [fn(x[1,:], x[2,:], x[3,:]) for x in X]
+
+"""
+And finally we create the workout and start the training.
+We use Mean Square Error loss and SGD as the optimizer.
+"""
+workout = Workout(model, mse, SGD())
+fit!(workout, zip(X,Y), epochs=10)
+
+@printf "%.1fx + %.1fy  + %.1fz + %.1f\n" model.params.w... model.params.b...
--- src/losses.jl
+++ src/losses.jl
@@ -1,5 +1,6 @@
 
 export mse, mae
 
-mse(y_pred, y) = mean((y_pred - y).*(y_pred - y))
-mae(y_pred, y) = mean(abs.(y_pred - y))
+mae(y_pred, y) = mean(abs.(mat(y_pred) .- mat(y)))
+
+mse(y_pred, y) = mean((mat(y_pred) .- mat(y)).^2)
