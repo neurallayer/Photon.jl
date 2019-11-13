b12e64635cbe31866584340f8d4f398bcdefe66d
neurallayer <peter@jbaron.com>
2019-11-13 15:00:11 +0100

    Improved VOC 2007 example

--- examples/voc2007.jl
+++ examples/voc2007.jl
@@ -1,42 +1,63 @@
 using Photon, LightXML
 
+# This example uses the VOC2007 image dataset. Rather then using segmentation,
+# this example uses classification. Without a GPU, this training will be slow.
+#
+# You can read more about this dataset at:
+#    http://host.robots.ox.ac.uk/pascal/VOC/voc2007/
+#
+# And you can download the dataset from:
+#    http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+
+
+# Change location below to where you extracted the VOC2007 dataset
 const VOC_DIR = "/home/peter/data/VOCdevkit/VOC2007/"
+
+# Shouldn't need to change anything below this line
 const IMAGEDIR = joinpath(VOC_DIR,"JPEGImages/")
 const LABELDIR = joinpath(VOC_DIR,"Annotations/")
 const LABELS = ["chair", "diningtable", "person", "boat", "pottedplant", "car", "sofa",
           "sheep", "cat", "aeroplane", "dog", "train", "bottle", "bird", "bicycle",
           "horse", "cow", "tvmonitor", "motorbike", "bus"]
 
-# Get all the image filenames
-images = readdir(IMAGEDIR)
 
 """
 The labels are stored in XML files, so some parsing is required to retrieve
 and transform them into onehot encodings. A single image can have multiple
 classes (like it contains a car AND and bicycle).
 """
-function get_labels(images)
+function get_labels(files::Vector{String}):: Vector{Array}
     labels = []
-    for f in images
+    for f in files
         bf = f[1:end-4]
         filename = joinpath(LABELDIR, bf * ".xml")
+
+        # Get the class labels for this image. These functions are from
+        # the excellent LightXML package
         xroot = root(parse_file(filename))
         objects = get_elements_by_tagname(xroot, "object")
         cls = content.(find_element.(objects, "name"))
+
+        # Now onehot encode the labels
         oh = onehot(unique(cls), LABELS, getContext().dtype)
         push!(labels, oh)
     end
     labels
 end
 
-labels = get_labels(images)
-images = joinpath.(IMAGEDIR, images)
+# Get all the image filenames
+files = readdir(IMAGEDIR)
+@info "Found $(length(files)) image files"
 
+labels = get_labels(files)
+@info "Parsed $(length(labels)) label files"
+
+images = joinpath.(IMAGEDIR, files)
 ds = ImageDataset(images, labels, resize=(200, 200))
 
 dl = Dataloader(ds, 16)
 
-# We create a simple convolutional network
+# Create a simple convolutional network
 model = Sequential(
     Conv2D(16, 3, relu),
     Conv2D(64, 3, relu),
@@ -48,3 +69,4 @@ model = Sequential(
 workout = Workout(model, mse)
 
 fit!(workout, dl, epochs=10)
+@info "Finished training for $(workout.epochs) epochs"
